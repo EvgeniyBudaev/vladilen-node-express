cd4d2a261382d4a1ec73005397fe76b50981b3f4
Evgeniy Budaev <budaev.e@gmail.com>
2021-08-16 08:56:58 +0300

    Модель корзины

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,52 @@
-class Cart {
-  add() {}
+const path = require('path');
+const fs = require('fs');
 
-  fetch() {}
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+  )
+
+class Cart {
+  static async add(course) {
+    const cart = await Cart.fetch();
+    const idx = cart.courses.findIndex(c => c.id === course.id);
+    const candidate = cart.courses[idx];
+
+    if (candidate) {
+      // курс уже есть
+      candidate.count++;
+      cart.courses[idx] = candidate;
+    } else {
+      // нужно добавить курс
+      course.count = 1;
+      cart.courses.push(course);
+    }
+
+    cart.price += +course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(cart), error => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      })
+    });
+  }
+
+  static async fetch() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(p, 'utf-8', (error, content) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(JSON.parse(content));
+        }
+      })
+    })
+  }
 }
 
 module.exports = Cart
